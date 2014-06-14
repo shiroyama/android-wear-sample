88bcde711b35debe58ee5cc5e3f1b447645e8b9b
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2014-06-14 13:55:25 +0900

    Android Wear Sample

---
+++ app/src/main/java/us/shiroyama/android/myapplication/MyActivity.java
@@ -0,0 +1,49 @@
+package us.shiroyama.android.myapplication;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.os.Bundle;
+import android.preview.support.v4.app.NotificationManagerCompat;
+import android.preview.support.wearable.notifications.WearableNotifications;
+import android.support.v4.app.NotificationCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentTitle("this is title")
+                .setContentText("this is text")
+                .setSmallIcon(R.drawable.ic_launcher);
+
+        Notification notification = new WearableNotifications.Builder(builder)
+                .build();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(1, notification);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
